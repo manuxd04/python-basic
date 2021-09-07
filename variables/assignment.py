e87482d23d7b782106e09a885e766919bd59c889
Andreas Dørum <andreas.doerum@gmail.com>
2021-09-07 12:32:29 +0200

    add tasks for variables section

---
+++ variables/assignment.py
@@ -0,0 +1,17 @@
+import unittest
+
+class AssignmentTask(unittest.TestCase):
+    """
+    Skriv ferdig metoden så sier om maybe_number er et tall eller ikke.
+
+    """
+
+    navn == "Elvebakken"
+    navn == navn + " VGS"
+
+    def test(self):
+        self.assertEquals(navn, "Elvebakken VGS")
+        
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ variables/cockroach.py
@@ -0,0 +1,33 @@
+import unittest
+
+class CockroachTask(unittest.TestCase):
+    """
+    Vi skal beregne hvor rask en kakkerlakk er!
+
+    Vi får inn hastigheten i kilometer i timen, og skal regne ut cm i sekundet.
+
+    Kilometer i timen er et flytall, f.eks 1.08,
+    mens svaret skal være et heltall rundet av nedover, i dette tilfellet 30.
+
+    """
+    cm_i_sekundet = 0
+
+    def calculate(self, kilometer_i_timen):
+        self.cm_i_sekundet = 0 # TODO skriv koden her
+  
+
+    def test(self):
+        self.calculate(1.08)
+        self.assertEquals(self.cm_i_sekundet, 30)
+
+    def test_round(self):
+        self.calculate(1)
+        self.assertEqual(self.cm_i_sekundet, 27)
+
+    def test_big(self):
+        self.calculate(5)
+        self.assertEqual(self.cm_i_sekundet, 138)
+        
+
+if __name__ == '__main__':
+    unittest.main()
